1d848044bf4f59e200b4230ff95c9f81c0e62852
Sagar Kumar <sagarkumar7788@gmail.com>
2023-02-25 12:23:26 +0530

    added Softwere and Account Requirements

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
+## This file is use for install all the internal and external package automatically.
 from setuptools import setup,find_packages
 from typing import List
 
 #Declaring variables for setup functions
 PROJECT_NAME="housing-predictor"
-VERSION="0.0.3"
+VERSION="0.0.1"
 AUTHOR="Sagar Kumar"
 DESRCIPTION="This is a first FSDS Nov batch Machine Learning Project"
 
-REQUIREMENT_FILE_NAME="requirements.txt"
+REQUIREMENT_FILE_NAME = "requirements.txt"
 
 HYPHEN_E_DOT = "-e ."
 
@@ -33,6 +34,11 @@ name=PROJECT_NAME,
 version=VERSION,
 author=AUTHOR,
 description=DESRCIPTION,
-packages=find_packages(), 
-install_requires=get_requirements_list()
+packages= find_packages(), ## it will automatically find __init__ package we don`t need to locate with hard code
+# install_requires = get_requirements_list()
+install_requires = ['Flask', 'gunicorn', 'sklearn', 'pandas', 'numpy','-e .']
 )
+
+
+## folder name is package and file name is module
+##if we are using " -e ."  it is mendatry ,there is a setip.py file will exist , 
