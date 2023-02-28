fae9edba8a5d64a791ae54c2d6c4d695a5bdc28e
Sagar Kumar <sagarkumar7788@gmail.com>
2023-03-01 00:51:38 +0530

    Folder structure completed

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_requirements_list()->List[str]:
     of libraries mentioned in requirements.txt file
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        requirement_list = requirement_file.readlines()
+        requirement_list = requirement_file.readline()
         requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
         if HYPHEN_E_DOT in requirement_list:
             requirement_list.remove(HYPHEN_E_DOT)
